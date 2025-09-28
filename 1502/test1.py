7db37040d4f7215f1c53a8e2c41ba8284d3fd8bd
Tim Churchward <git@timchurchward.com.au>
2025-09-29 09:39:16 +1000

    initial commit

---
+++ 1502/test1.py
@@ -0,0 +1,15 @@
+__metadata__ = {
+    "course": "Tim's Sandpit",
+    "slug": "datacamp-light-exercises",
+    "placement": "example"
+}
+
+
+class Book:
+    def __init__(self):
+        self.title = "Atomic Habits"
+        self.author = "James Clear"
+        self.page_count = 234
+
+
+book = Book()
