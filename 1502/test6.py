f03b86dd4a7108c245cb8a001ebc6b997e01750f
Tim Churchward <git@timchurchward.com.au>
2025-09-30 18:59:08 +1000

    changed some files to use single quotation marks for the strings

--- 1502/test6.py
+++ 1502/test6.py
@@ -1,7 +1,8 @@
 __metadata__ = {
     course: "Tim's Sandpit",
     page: "dcl-6",
-    placement: ["example", "test"]
+    placement: ['example', 'test']
 }
 
+# Now with single quotation marks for the list
 print("This exercise will go in two places")
